b1e4ff742da6389cfa947cbdfbc40c5893ef0499
Harsh Vyapari (harshv5094) <harshvy5094@proton.me>
2026-06-11 17:30:38 +0530

    feat(winget-pkg): a script to auto install `winget` packages

---
+++ static/scripts/winget-pkg.ps1
@@ -0,0 +1,47 @@
+# List of packages
+$packages = @(
+  # CLI and TUI Tools
+  "Git.Git",
+  "GitHub.cli ",
+  "GnuPG.GnuPG",
+  "GnuPG.Gpg4Win",
+  "7zip.7zip",
+  "sharkdp.fd",
+  "sharkdp.bat",
+  "topgrade-rs.topgrade",
+  "eza-community.eza",
+  "junegunn.fzf",
+  "BurntSushi.ripgrep.MSVC",
+  "cURL.cURL"
+  "ajeetdsouza.zoxide",
+  "Starship.Starship",
+
+  # Sharing
+  "LocalSend.LocalSend",
+
+  # Browser
+  "Mozilla.Firefox",
+
+  # Notebook app
+  "Obsidian.Obsidian",
+
+  #Media
+  "VideoLAN.VLC",
+
+  # Neovim and its dependencies
+  "Neovim.Neovim",
+  "tree-sitter.tree-sitter-cli",
+
+  # Programming Languages
+  "OpenJS.NodeJS.LTS",
+  "GoLang.Go"
+)
+
+foreach ($pkg in $packages)
+{
+  Write-Host "Installing $pkg..." -ForegroundColor Cyan
+  winget install --id $pkg --silent --accept-package-agreements
+}
+
+Write-Host "Installing posix packages..."
+winget install --id=BrechtSanders.WinLibs.POSIX.UCRT -e
